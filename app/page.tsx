3082ac8cd5fd284276cb0b4f978a1a33bd8f7f2a
Mohammed <childofgod989@gmail.com>
2025-07-24 16:44:05 +0000

    case study import removed

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { ProjectsSection } from "@/components/projects-section";
 import { AboutSection } from "@/components/about-section";
 import { ContactSection } from "@/components/contact-section";
 import { Footer } from "@/components/footer";
-import { CaseStudySection as CaseStudy } from "@/components/case-study";
+// Remove this line: import { CaseStudySection as CaseStudy } from "@/components/case-study";
 
 export default function Home() {
   return (

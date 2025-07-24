c2620606ca784fb12efd37f4b29ed4f01bc1d4bc
Mohammed <childofgod989@gmail.com>
2025-07-24 17:35:12 +0000

    lenis smooth scroll fixed

--- components/smooth-scroll.tsx
+++ components/smooth-scroll.tsx
@@ -12,8 +12,7 @@ export const SmoothScroll = ({ children }: SmoothScrollProps) => {
     const lenis = new Lenis({
       duration: 1.2,
       easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
-      direction: "vertical",
-      gestureDirection: "vertical",
+      // Remove direction and gestureDirection - not supported in this version
       smooth: true,
       smoothTouch: false,
       touchMultiplier: 2,
